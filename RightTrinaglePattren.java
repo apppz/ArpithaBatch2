bc64e3e8c338c1d65beb3a8da8e22bf13a7191f3
apppz <91308757+apppz@users.noreply.github.com>
2022-06-17 09:45:25 +0530

    Add files via upload

---
+++ RightTrinaglePattren.java
@@ -0,0 +1,19 @@
+package com.xworkz.xworkz2.PattrenPrograms.Pattren;
+
+public class RightTrinaglePattren {
+  
+	public static void main(String args[])   
+	{     
+	int i, j, row=6;   
+  
+	for(i=0; i<row; i++)   
+	{    
+	for(j=0; j<=i; j++)   
+	{    
+	System.out.print("* ");   
+	}     
+	System.out.println();   
+	}   
+	}   
+	}  
+
